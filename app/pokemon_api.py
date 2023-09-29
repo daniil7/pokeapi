4abb82e052aa94dcaf0cebf9d1d3740f24622aa0
daniil7 <daniil.k@mail.ru>
2023-09-30 02:13:53 +0500

    request all pokemons

--- app/pokemon_api.py
+++ app/pokemon_api.py
@@ -5,8 +5,15 @@ API_ROUTE = "https://pokeapi.co/api/v2/"
 class APIRequestException(Exception):
     pass
 
+def request_count():
+    r = requests.get(API_ROUTE+'pokemon?limit=1')
+    if r.status_code != 200:
+        raise APIRequestException('could not request count of pokemons, status code: '+r.status_code)
+    return r.json()['count']
+
 def request_pokemons():
-    r = requests.get(API_ROUTE+'pokemon?limit=100')
+    count = request_count()
+    r = requests.get(API_ROUTE+'pokemon?limit='+str(count))
     if r.status_code != 200:
         raise APIRequestException('could not request pokemons, status code: '+r.status_code)
     return r.json()['results']
