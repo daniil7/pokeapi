23d3cec284ee513174e9efeabedc21fbfdc33cb1
daniil7 <daniil.k@mail.ru>
2023-09-30 01:38:42 +0500

    pokemons request, pokemons search, certain pokemon info page

--- app/pokemon_api.py
+++ app/pokemon_api.py
@@ -1,3 +1,18 @@
 import requests
 
 API_ROUTE = "https://pokeapi.co/api/v2/"
+
+class APIRequestException(Exception):
+    pass
+
+def request_pokemons():
+    r = requests.get(API_ROUTE+'pokemon?limit=100')
+    if r.status_code != 200:
+        raise APIRequestException('could not request pokemons, status code: '+r.status_code)
+    return r.json()['results']
+
+def request_pokemon(pokemon_name: str):
+    r = requests.get(API_ROUTE+'pokemon/'+pokemon_name)
+    if r.status_code != 200:
+        raise APIRequestException('could not request pokemon '+pokemon_name+', status code: '+r.status_code)
+    return r.json()
--- app/views.py
+++ app/views.py
@@ -1,10 +1,45 @@
-from flask import render_template
+from flask import render_template, make_response
 from app import app
+
+from app.pokemon_api import request_pokemons, request_pokemon, APIRequestException
+import json
+
+def api_error_response(error_message):
+    return {'error', error_message}
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = { 'nickname': 'Miguel' }
     return render_template("index.html",
-        title = 'Home',
-        user = user)
+        title = 'Home')
+
+@app.route('/pokemon/<pokemon_name>')
+def pokemon(pokemon_name):
+    pokemon = request_pokemon(pokemon_name)
+    return render_template(
+        "pokemon.html",
+        title = 'Pokemon',
+        pokemon = pokemon,
+        pokemon_name = pokemon_name)
+
+@app.route('/api/pokemon')
+def api_pokemons():
+    try:
+        return app.response_class(
+            response=json.dumps(request_pokemons()),
+            status=200,
+            mimetype='application/json'
+        )
+    except APIRequestException as e:
+        return api_error_response(e.message)
+
+@app.route('/api/pokemon/<pokemon_name>')
+def api_certain_pokemon(pokemon_name):
+    try:
+        return app.response_class(
+            response=json.dumps(request_pokemon(pokemon_name)),
+            status=200,
+            mimetype='application/json'
+        )
+    except APIRequestException as e:
+        return api_error_response(e.message)
