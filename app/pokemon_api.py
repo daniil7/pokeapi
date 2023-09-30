8ad8f99d82ef32ed5b044950c387821aaf66432c
daniil7 <daniil.k@mail.ru>
2023-09-30 22:19:59 +0500

    cache module

--- app/pokemon_api.py
+++ app/pokemon_api.py
@@ -1,4 +1,5 @@
 import requests
+from app.cache_module import read_cache, write_cache
 
 API_ROUTE = "https://pokeapi.co/api/v2/"
 
@@ -12,14 +13,32 @@ def request_count():
     return r.json()['count']
 
 def request_pokemons():
+
+    cache = read_cache('pokemons')
+    if cache is not None:
+        return cache
+
     count = request_count()
     r = requests.get(API_ROUTE+'pokemon?limit='+str(count))
     if r.status_code != 200:
         raise APIRequestException('could not request pokemons, status code: '+r.status_code)
-    return r.json()['results']
+    response_data = r.json()['results']
+
+    write_cache('pokemons', response_data)
+
+    return response_data
 
 def request_pokemon(pokemon_name: str):
+
+    cache = read_cache('pokemon-'+pokemon_name)
+    if cache is not None:
+        return cache
+
     r = requests.get(API_ROUTE+'pokemon/'+pokemon_name)
     if r.status_code != 200:
         raise APIRequestException('could not request pokemon '+pokemon_name+', status code: '+r.status_code)
-    return r.json()
+    response_data = r.json()
+
+    write_cache('pokemon-'+pokemon_name, response_data)
+
+    return response_data
