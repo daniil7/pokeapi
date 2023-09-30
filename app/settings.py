45367984ce90e9331c6ec32e01a6e9ad50de5f15
daniil7 <daniil.k@mail.ru>
2023-09-30 23:43:37 +0500

    fix settings

--- app/settings.py
+++ app/settings.py
@@ -3,6 +3,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 CONFIG = {
-    'CACHE_EXPIRATION': int(os.environ.get('CACHE_EXPIRATION')) \
-        if os.environ.get('CACHE_EXPIRATION') is not None else 86400,
+    'CACHE_EXPIRATION': int(os.environ.get('CACHE_EXPIRATION', '86400'))
 }
