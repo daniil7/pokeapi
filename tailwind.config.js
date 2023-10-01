95f561409aa6842dd430c28f37e2bdf75cea8cad
daniil7 <daniil.k@mail.ru>
2023-10-01 15:40:01 +0500

    fix tailwindcss config to handle js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/templates/**/*.html'],
+  content: [
+      './app/templates/**/*.html',
+      './app/src/js/**/*.js',
+  ],
   theme: {
     extend: {},
   },
