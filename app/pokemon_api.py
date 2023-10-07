3339e5d234df26b1e5fc5853cd2ca740d0774cbd
daniil7 <daniil.k@mail.ru>
2023-10-07 09:46:03 +0500

    pokemon stats counts

--- app/pokemon_api.py
+++ app/pokemon_api.py
@@ -11,7 +11,7 @@ class APIRequestException(Exception):
 def request_count():
     r = requests.get(API_ROUTE+'pokemon?limit=1')
     if r.status_code != 200:
-        raise APIRequestException('could not request count of pokemons, status code: '+r.status_code)
+        raise APIRequestException('could not request count of pokemons, status code: ' + str(r.status_code))
     return r.json()['count']
 
 def request_pokemons():
@@ -23,7 +23,7 @@ def request_pokemons():
     count = request_count()
     r = requests.get(API_ROUTE+'pokemon?limit='+str(count))
     if r.status_code != 200:
-        raise APIRequestException('could not request pokemons, status code: '+r.status_code)
+        raise APIRequestException('could not request pokemons, status code: ' + str(r.status_code))
     response_data = r.json()['results']
 
     write_cache('pokemons', response_data)
@@ -38,7 +38,7 @@ def request_pokemon(pokemon_name: str):
 
     r = requests.get(API_ROUTE+'pokemon/'+pokemon_name)
     if r.status_code != 200:
-        raise APIRequestException('could not request pokemon '+pokemon_name+', status code: '+r.status_code)
+        raise APIRequestException('could not request pokemon '+pokemon_name+', status code: ' + str(r.status_code))
     response_data = r.json()
 
     write_cache('pokemon-'+pokemon_name, response_data)
--- app/views.py
+++ app/views.py
@@ -32,13 +32,22 @@ def battle(user_pokemon_name):
     user_pokemon = request_pokemon(user_pokemon_name)
     enemy_pokemon_name = random.choice(request_pokemons())['name']
     enemy_pokemon = request_pokemon(enemy_pokemon_name)
+    user_pokemon_stats = {}
+    for stat in user_pokemon['stats']:
+        user_pokemon_stats[stat['stat']['name']] = stat['base_stat']
+    enemy_pokemon_stats = {}
+    for stat in enemy_pokemon['stats']:
+        enemy_pokemon_stats[stat['stat']['name']] = stat['base_stat']
+
     return render_template(
         "battle.html",
         title = 'Pokemon',
         user_pokemon = user_pokemon,
         user_pokemon_name = user_pokemon_name,
         enemy_pokemon = enemy_pokemon,
-        enemy_pokemon_name = enemy_pokemon_name)
+        enemy_pokemon_name = enemy_pokemon_name,
+        user_pokemon_stats = user_pokemon_stats,
+        enemy_pokemon_stats = enemy_pokemon_stats)
 
 @app.route('/api/pokemon')
 def api_pokemons():
