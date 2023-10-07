85b4b727e74d2802977b72856e7ee58e5920284e
daniil7 <daniil.k@mail.ru>
2023-10-07 13:54:06 +0500

    show pokemons stats on index page

--- app/src/js/api.js
+++ app/src/js/api.js
@@ -5,6 +5,9 @@ export default class API
     static async retrievePokemons() {
         return await (await fetch('/api/pokemon')).json();
     }
+    static async retrievePokemon(pokemon_name) {
+        return await (await fetch('/api/pokemon/'+pokemon_name)).json();
+    }
     static async retrieveSprite(pokemon_name) {
         return await (await fetch('/api/pokemon-sprite/'+pokemon_name)).text();
     }
