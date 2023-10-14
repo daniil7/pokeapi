128725273cff5ae3384e0d79b095bad40516faac
daniil7 <daniil.k@mail.ru>
2023-10-15 00:10:33 +0500

    run tests script

---
+++ run_test.py
@@ -0,0 +1,48 @@
+import importlib, os
+
+import sys
+import inspect
+from setuptools import find_packages
+from pkgutil import iter_modules
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+TESTS_DIR = 'tests'
+
+def find_modules(path):
+    modules = {}
+    for pkg in find_packages(path):
+        pkgpath = path + '/' + pkg.replace('.', '/')
+        for _, name, ispkg in iter_modules([pkgpath]):
+            if not ispkg:
+                modules[name] = TESTS_DIR + '/' + pkg + '/' + name + '.py'
+    return modules
+
+test_modules = find_modules(TESTS_DIR)
+
+print("\n-------------------------------\n")
+
+for module_name in test_modules:
+    loader = importlib.machinery.SourceFileLoader(module_name, test_modules[module_name])
+    module = loader.load_module()
+    try:
+        result, message = module.Test.do()
+    except Exception as e:
+        print("Test " + module_name + f" {bcolors.WARNING}FAILED WITH EXCEPTION{bcolors.ENDC}")
+        print(str(e))
+    if result:
+        print("Test " + module_name + f" {bcolors.OKGREEN}PASSED{bcolors.ENDC}")
+    else:
+        print("Test " + module_name + f" {bcolors.WARNING}FAILED WITH MESSAGE{bcolors.ENDC}")
+        print(message)
+
+print()
